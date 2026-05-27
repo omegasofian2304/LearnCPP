5807b21d3ee055fb93d6afde3dd1cb0a9b479f1b
sofian.hussein@eduvaud.ch <sofian.hussein@eduvaud.ch>
2026-05-27 10:15:53 +0200

    feat: add verify wallet

--- LearnCPP/LearnCPP.cpp
+++ LearnCPP/LearnCPP.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+void verifyWallet(int totalWallet, int totalProduct) {
+    if (totalWallet < totalProduct) {
+        int difference = totalProduct - totalWallet;
+
+        std::cout << "You are " << difference << " francs short." << std::endl;
+    }
+    else
+    {
+        int difference = totalWallet - totalProduct;
+        std::cout << "You can buy it" << std::endl;
+        std::cout << difference << ".- remaining in your Wallet";
+    }
+}
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+    int broWallet = 5;
+    int myWallet = 10;
+	int totalWallet = broWallet + myWallet;
+	int productPrice = 7;
+
+    verifyWallet(totalWallet, productPrice);
+
     return 0;
 }
